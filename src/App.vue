7549770396a10ff76b55fe0aebb011e59589ae42
unknown <guilherme@testingcompany.com.br>
2021-05-05 21:23:27 -0300

    dev geral

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Footer from './components/includes/footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Footer
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif ;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #eeeaea;
+  background-color: #314149;
+}
+</style>
